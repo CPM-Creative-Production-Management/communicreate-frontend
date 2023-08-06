c0ecf4c1d4a5502812785a2f65e9278c3905ba58
Fabiha Tasneem <1805072@ugrad.cse.buet.ac.bd>
2023-08-06 14:48:19 +0600

    payment page added

--- src/components/pages/PaymentPage.js
+++ src/components/pages/PaymentPage.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {Message, Step} from "semantic-ui-react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Dashboard } from "../fragments/Dashboard";
+import {Estimations} from "../fragments/Estimations";
+import Fab from '@mui/material/Fab';
+import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
+import "./pages.css"
 
 const PaymentPage = () => {
+
     return (
-        <div>PaymentPage</div>
+        <div>
+
+            <div class="ui ordered steps">
+                <div class="completed step">
+                    <div class="content">
+                        <div class="title">Finalize</div>
+                        <div class="description">Your estimation is finalized!</div>
+                    </div>
+                </div>
+                <div class="active step">
+                    <div class="content">
+                        <div class="title">Billing</div>
+                        <div class="description">Enter billing information</div>
+                    </div>
+                </div>
+                <div class="disabled step">
+                    <div class="content">
+                        <div class="title">Confirm Payment</div>
+                        <div class="description">Initialize your transaction</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default PaymentPage
