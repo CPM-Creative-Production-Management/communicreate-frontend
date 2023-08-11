90ede16776d0c3a61e25206ac444037d22e064ab
anupbhowmik <anupbhowmik.1998@gmail.com>
2023-08-11 15:05:23 +0600

    regular api req updated

--- src/components/api/regularApiRequest.js
+++ src/components/api/regularApiRequest.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import {setLoading, showToast} from "../../App";
-import {useEffect, useState} from "react";
+import { setLoading, showToast } from "../../App";
+import { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
 
 
-export const regularApiRequest = async ({url, method, reqBody = {}}) => {
+export const regularApiRequest = async ({ url, method, reqBody = {} }) => {
     const cookies = new Cookies();
 
     const requestHeaders = {
@@ -16,29 +16,42 @@ export const regularApiRequest = async ({url, method, reqBody = {}}) => {
     console.log('reqBody', reqBody)
     console.log('requestHeaders', requestHeaders)
 
+    setLoading(true)
+
     try {
         if (method.toUpperCase() === "GET") {
-            const response = await axios.get(url, {headers: requestHeaders})
+            const response = await axios.get(url, { headers: requestHeaders })
             console.log('resp', response.data)
+            setLoading(false)
             return response
 
         } else if (method.toUpperCase() === "POST") {
-            const response = await axios.post(url, reqBody, {headers: requestHeaders})
+            const response = await axios.post(url, reqBody, { headers: requestHeaders })
             console.log('resp', response.data)
+            setLoading(false)
+
             return response
 
         } else if (method.toUpperCase() === "PUT") {
-            const response = await axios.put(url, reqBody, {headers: requestHeaders})
+            const response = await axios.put(url, reqBody, { headers: requestHeaders })
             console.log('resp', response.data)
+            setLoading(false)
+
             return response
         } else if (method.toUpperCase() === "DELETE") {
-            const response = await axios.delete(url, {headers: requestHeaders})
+            const response = await axios.delete(url, { headers: requestHeaders })
+
             console.log('resp', response.data)
+            setLoading(false)
+
             return response
         }
 
     } catch (error) {
         console.log(error)
+        setLoading(false)
+
         showToast("Error fetching data", "error")
     }
+
 }
