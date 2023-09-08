8a5a18979560d931f91e38dd234a002c82509d36
anupbhowmik <anupbhowmik.1998@gmail.com>
2023-09-08 21:55:09 +0600

    graphs added

--- src/components/fragments/Dashboard.js
+++ src/components/fragments/Dashboard.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import {Card, Grid, Icon} from "semantic-ui-react";
+import { Card, Grid, Icon } from "semantic-ui-react";
 
 
 import '../../global.css'
-import {DashboardCard} from "../cards/DashboardCard";
+import { DashboardCard } from "../cards/DashboardCard";
 import { useNavigate } from 'react-router-dom';
-import {Button} from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
+import GeneralChart from '../utils/GeneralChart'
 
 
 export const Dashboard = () => {
 
 
     let navigate = useNavigate();
+
+    const userData1 = [
+        {
+            id: 1,
+            year: 2016,
+            userGain: 80000,
+            userLost: 823,
+        },
+        {
+            id: 2,
+            year: 2017,
+            userGain: 45677,
+            userLost: 345,
+        },
+        {
+            id: 3,
+            year: 2018,
+            userGain: 78888,
+            userLost: 555,
+        },
+        {
+            id: 4,
+            year: 2019,
+            userGain: 90000,
+            userLost: 4555,
+        },
+        {
+            id: 5,
+            year: 2020,
+            userGain: 4300,
+            userLost: 234,
+        },
+    ];
 
 
     return (
@@ -26,17 +60,17 @@ export const Dashboard = () => {
                 <Grid.Row>
                     <Grid.Column>
                         <DashboardCard iconName='forward' colorName='teal' cardHeader='Ongoing Estimations'
-                                       cardDesc='These are the estimations that you have started but not yet completed. Negotiation is going on with the clients.'
-                                       count='5'/>
+                            cardDesc='These are the estimations that you have started but not yet completed. Negotiation is going on with the clients.'
+                            count='5' />
 
                     </Grid.Column>
 
                     <Grid.Column>
 
                         <DashboardCard iconName='check' colorName='green' cardHeader='Finalized Estimations'
-                                       cardDesc='These are the estimations that have been accepted.
+                            cardDesc='These are the estimations that have been accepted.
                                     You can now start working on the projects.'
-                                       count='3'/>
+                            count='3' />
 
                     </Grid.Column>
 
@@ -45,13 +79,49 @@ export const Dashboard = () => {
 
                         <DashboardCard iconName='close' colorName='red' cardHeader='Rejected Estimations'
 
-                                       cardDesc='These are the estimations that have been rejected by the clients or by you.'
-                                       count='2'/>
+                            cardDesc='These are the estimations that have been rejected by the clients or by you.'
+                            count='2' />
 
 
                     </Grid.Column>
 
-                    <Grid.Column></Grid.Column>
+
+                </Grid.Row>
+
+
+            </Grid>
+
+            <Grid className='ms-2' columns={2}>
+                <Grid.Row>
+
+                    <Grid.Column>
+                        <Card fluid>
+
+                            <Card.Content header='Requests' />
+                            <Card.Content>
+                                <GeneralChart userData={userData1}
+                                    labelFieldName={'year'} dataFieldName={'userGain'}
+                                    type={'BAR'} color={'rgba(53, 162, 235, 0.5)'}
+                                />
+                            </Card.Content>
+
+                        </Card>
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <Card fluid>
+
+                            <Card.Content header='Estimations' />
+                            <Card.Content>
+                                <GeneralChart userData={userData1}
+                                    labelFieldName={'year'} dataFieldName={'userGain'}
+                                    type={'LINE'} color={'rgba(53, 162, 235, 0.5)'}
+                                />
+                            </Card.Content>
+
+                        </Card>
+                    </Grid.Column>
+
                 </Grid.Row>
             </Grid>
 
